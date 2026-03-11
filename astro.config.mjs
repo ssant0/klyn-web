e489fd79a5987761e9def95440cfef099a7c0102
Manuel Samaniego <samaniegoserranom@gmail.com>
2026-03-11 15:47:57 -0700

    feature(seo) some configurations missing

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://www.klyn.com.mx',
   integrations: [sitemap()],
   output: 'static',
 });
